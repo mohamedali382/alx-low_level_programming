73d9d1bed71216cd280d850a0516da23a9385d1b
mohamedali382 <ma392213@gmail.com>
2023-09-28 06:59:08 -0700

    g

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-		putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 }
