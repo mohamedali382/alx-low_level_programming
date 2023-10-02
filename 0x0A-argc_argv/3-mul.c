92bd6fedcd247b19731519bbbd7b15450cc69bbd
mohamedali382 <ma392213@gmail.com>
2023-10-02 11:45:17 -0700

    j

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main -  that multiplies two numbers
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 
 		for (int i = 1; i < argc; i++)
 		{
-			res *= strtol(argv[i], 0, 10);
+			res *= strtol(argv[i], NULL, 10);
 		}
 		printf("%s\n", res);
 		return (0);
