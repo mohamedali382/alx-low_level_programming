04bec68d5fbb6ea9da8a71be1c9f3d04e219018f
mohamedali382 <ma392213@gmail.com>
2023-10-02 12:38:36 -0700

    j

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		for (c = argv[argv]; *c; c++)
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-		sum += atol(argv[argc]);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
 	return (0);
