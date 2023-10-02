8a64326643a7e6908f2a3854e14ea64c4f2ec8a6
mohamedali382 <ma392213@gmail.com>
2023-10-02 10:00:07 -0700

    H

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print string name
+ * @argc: int
+ * @argv: list
+ * Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
