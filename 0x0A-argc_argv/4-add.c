2081f85bba3ddc3ce2d55a95b9a535d15d664feb
mohamedali382 <ma392213@gmail.com>
2023-10-02 12:17:33 -0700

    j

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0 or 1
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, c = 0;
+
+	for (int i = 0; i < argc; i++)
+	{
+		sum += atol(argv[i]);
+		if (argv[i] >= 'a' && argv[i] <= 'z')
+			c++;
+	}
+
+	if (c > 0)
+	{
+		printf("Error");
+		return (1);
+	}
+	else if (argc == 0)
+	{
+		printf("0");
+	}
+	else
+	{
+		printf("%d\n", sum);
+	}
+	return (0);
+}
