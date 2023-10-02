8f8e47e757cdc1977527f0ca55517ee12f873cdc
mohamedali382 <ma392213@gmail.com>
2023-10-02 11:54:58 -0700

    J

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 0)
-	{
-		printf("Error");
-		return (1);
-	}
-	else if (argc == 3)
+	if (argc == 3)
 	{
 		int res;
 
 		res = atoi(argv[1]) * atoi(argv[2]);
 
 		printf("%s\n", res);
-		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
 	}
 }
