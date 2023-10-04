28f1387a29888d8d8d8133ed722c75465ed79c0f
mohamedali382 <ma392213@gmail.com>
2023-10-04 14:20:31 -0700

    j

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * len - find the length of the stirng
+ * @str: input string
+ * Return: length of string
+*/
+
+int len(char *str)
+{
+	for (int i = 0; str[i]; i++)
+		return (i);
+}
+
+/**
+ * str_concat -  concatenates two strings
+ * @s1: input 1st string
+ * @s2: input 2nd string
+ * Return: 0 or pointer
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int l1 = len(s1);
+	int l2 = len(s2);
+	int i;
+	char *s = malloc((l1 + l2) * sizeof(char) + 1);
+
+		if (s == 0)
+			return (0);
+		for (i = 0; i <= (l1 + l2); i++)
+		{
+			if (i < l1)
+			m[i] = s1[i];
+
+			else
+				m[i] = s2[i - l1];
+		}
+		return (m);
+}
