bae5d9fb71c0ed2099555b451f5f6fd796044812
mohamedali382 <ma392213@gmail.com>
2023-10-04 13:43:24 -0700

    j

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,23 @@
 
 char *_strdup(char *str)
 {
+	int i = 0, size = 0;
+	char *m;
+
 	if (str == NULL)
+		return (0);
+	for (; *str[size] != '\0'; size++)
+		;
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
 	{
 		return (0);
 	}
-
 	else
 	{
-		char *ptr = *str;
-
-		*ptr = (char *)malloc(sizeof(char));
-
-		free(ptr);
-		return (ptr);
+		for (; i < size; i++)
+			m[i] = str[i];
 	}
+	return (m);
 }
