b1db861071293aa28dda285cc8b584de13a92b11
mohamedali382 <ma392213@gmail.com>
2023-10-04 14:30:59 -0700

    J

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,12 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *s = malloc((l1 + l2) * sizeof(char) + 1);
 
-		if (s == 0 || l1 == 0 || l2 == 0)
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+
+		if (s == 0)
 			return (0);
 		for (i = 0; i <= (l1 + l2); i++)
 		{
