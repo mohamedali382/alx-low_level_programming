5e01eed4d28c039eb617215634dd3fc79e84bf9b
mohamedali382 <ma392213@gmail.com>
2023-10-10 16:15:40 -0700

    J

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -71,7 +71,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog->name = copy(dog->name, name);
 	dog->age = age;
-	dog->owner = copy(owner->owner, owner);
+	dog->owner = copy(dog->owner, owner);
 
 	return (dog);
 }
