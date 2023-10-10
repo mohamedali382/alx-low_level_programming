eff436eb1c1aae4f70c6364bb747cf8dc4567768
mohamedali382 <ma392213@gmail.com>
2023-10-10 16:29:45 -0700

    J

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,11 @@ struct dog
 	float age;
 	char *owner;
 };
-void free_dog(dog_t *d);
-dog_t *new_dog(char *name, float age, char *owner);
+
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
