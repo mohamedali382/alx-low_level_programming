ba5c8ee81904abaed74688faf590460fed0331e7
mohamedali382 <ma392213@gmail.com>
2023-10-19 15:11:56 -0700

    j

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "limits.h"
+
+/**
+ * _str - return length of string
+ * @s: input string
+ * Return: 0 or string length
+*/
+
+int _str(char *s)
+{
+	int i = 0;
+
+	if (!s)
+	return (0);
+	while (*s++)
+	i++;
+	return (i);
+}
+/**
+ * print_list - print the linked list
+ * @h: pointer of 1st node
+ * Return: size of list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _str(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
