4725ef99e61a2cd316524e38dbd36d56cf5163e9
mohamedali382 <ma392213@gmail.com>
2023-10-19 15:50:25 -0700

    get

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - add a new node in list
+ * @head: 1st node in list
+ * @str: new node
+ * Return: size of list
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_H = malloc(sizeof(list_t));
+
+	if (!head || !str)
+		return (NULL);
+	if (str)
+	{
+		new_H->str = strdup(str);
+		if (!new_H->str)
+		{
+			free(new_H);
+			return (NULL);
+		}
+		new_H->len = _str(new_H->head);
+	}
+	new_H->next = *head;
+	*head = new_H;
+	return (new_H);
+}
