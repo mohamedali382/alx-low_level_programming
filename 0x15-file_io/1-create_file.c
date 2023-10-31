f9e68e179d79e173d83f67a2550e4984f20ef417
mohamedali382 <ma392213@gmail.com>
2023-10-31 14:46:13 -0700

    J

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	fb = write(count, text_content, strlen(text_content));
+	fb = write(count, text_content, sizeof(text_content));
 
 	if (fb < 0)
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -41,7 +41,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	w = write(fb, text_content, strlen(text_content);
+	w = write(fb, text_content, sizeof(text_content);
 	if (w < 0)
 		return (-1);
 	close(fb);
