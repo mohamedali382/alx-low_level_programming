25dac11c20fde65da063ceedaf8fd5ee812446e7
mohamedali382 <ma392213@gmail.com>
2023-10-31 14:54:40 -0700

    J

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * strlen - return the length of string
+ * @c: input string
+ *
+ * Return: length of the input string
+*/
+
+int strlen(char *c)
+{
+	int i = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+
+/**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the created file
  * @text_content: length of the inputs in file
@@ -12,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fb, w;
 
-	fb = open(filenme, O_WRONLY | O_APPEND);
+	fb = open(filename, O_WRONLY | O_APPEND);
 
 	if (!filename || fb < 0)
 		return (-1);
@@ -23,7 +41,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	w = write(fb, text_content, sizeof(text_content));
+	w = write(fb, text_content, strlen(text_content));
 	if (w < 0)
 		return (-1);
 	close(fb);
